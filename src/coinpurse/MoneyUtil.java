521d2ab7f257f7d6aeda57d88fb501f308dedd7a
GDROM <Rommadon.t@ku.th>
2018-01-22 13:43:30 +0700

    change list to arraylist

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -1,5 +1,24 @@
 package coinpurse;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 public class MoneyUtil {
+	
+	public void sortCoins(){
+		ArrayList<Coin> coins = new ArrayList<Coin>();
+		coins.add( new Coin(10.0, "Baht") );
+		coins.add( new Coin(0.5, "Baht") );
+		coins.add( new Coin(1.0, "Baht") );
+	 
+		java.util.Collections.sort( coins );
+		printCoins( coins ); 
+		
+	}
+	
+	public void printCoins(ArrayList<Coin> coins){
+		coins.forEach(System.out::println);
+		
+	}
 
 }
