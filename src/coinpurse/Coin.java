4e05f3da2445c49b786c502811700a2063efa049
GDROM <Rommadon.t@ku.th>
2018-01-23 15:31:58 +0700

    fix javadoc of COIN

--- src/coinpurse/Coin.java
+++ src/coinpurse/Coin.java
@@ -1,29 +1,38 @@
 package coinpurse;
- 
-
-
-/**
+ /**
  *  
- *  
+ *  Coin represents value and currency.
  *  @author Rommadon Teedo 5810545025
  */
 public class Coin implements Comparable<Coin>{
     private double value;
 	private String currency;
-
 	
-    
+	/**
+	 * Constructor for coin with a value and currency
+	 * @param value of the coin
+	 * @param currency of the coin
+	 */
 	Coin(double value,String currency){
 		this.value = value;
 		this.currency = currency;
 		
     }
 
-  
+	/**
+	 * Return the value of coin
+	 * @return value of coin
+	 */
     public double getValue( ){ return value; }
+    /**
+	 * Return the currency of coin
+	 * @return currency of coin
+	 */
     public String getCurrency( ){ return currency; }
     
- 
+    /**
+	 * Two coin are equal if they have same value and currency.
+	 */
     public boolean equals(Object arg) {
     	if (arg == null) return false;
 		if (arg.getClass() != this.getClass() )
@@ -34,7 +43,10 @@ public class Coin implements Comparable<Coin>{
 		return false; 
 	}
 
-    
+    /**
+	 * Compare the value of coins.
+	 * @param coin
+	 */
    
     public int compareTo(Coin coin) { 
     	if(coin.getValue()-this.getValue() <0)
@@ -47,7 +59,10 @@ public class Coin implements Comparable<Coin>{
 		}
 		
 	}
-
+    /**
+	 * show value and currency
+	 * @return the sentence value and currency.
+	 */
     public String toString() {
        
     	return value + " " + currency;
