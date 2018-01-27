b597d5a1212bc46afb4b7f4759abc3e8b344d21b
GDROM <Rommadon.t@ku.th>
2018-01-27 14:21:53 +0700

    Fix issue coin compareTo

--- src/coinpurse/Coin.java
+++ src/coinpurse/Coin.java
@@ -49,10 +49,10 @@ public class Coin implements Comparable<Coin>{
 	 */
    
     public int compareTo(Coin coin) { 
-    	if(coin.getValue()-this.getValue() <0)
+    	if(this.getValue()-coin.getValue() <0)
 		{
 			return -1;
-		} else if(coin.getValue()-this.getValue() > 0){
+		} else if(this.getValue()-coin.getValue() > 0){
 			return 1;
 		} else { 
 			return 0;
