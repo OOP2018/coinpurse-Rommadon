68de54f9117cb674b2fc1e46285f3ff9c36a40e9
GDROM <Rommadon.t@ku.th>
2018-01-27 14:26:23 +0700

    Fix ident withdraw

--- src/coinpurse/Purse.java
+++ src/coinpurse/Purse.java
@@ -14,76 +14,76 @@ import java.util.Collections;
  *  @author Rommadon Teedo 5810545025
  */
 public class Purse {
-    /** Collection of objects in the purse. */
+	/** Collection of objects in the purse. */
 	List<Coin> money = new ArrayList<Coin>( );
-    
-    /** Capacity is maximum number of items the purse can hold.
-     *  Capacity is set when the purse is created and cannot be changed.
-     */
-    private final int capacity;
-    
-    /** 
-     *  Create a purse with a specified capacity.
-     *  @param capacity is maximum number of coins you can put in purse.
-     */
-    public Purse( int capacity ) {
-    	this.capacity=capacity;
 
-    }
+	/** Capacity is maximum number of items the purse can hold.
+	 *  Capacity is set when the purse is created and cannot be changed.
+	 */
+	private final int capacity;
 
-    /**
-     * Count and return the number of coins in the purse.
-     * This is the number of coins, not their value.
-     * @return the number of coins in the purse
-     */
-    public int count() { return money.size(); }
-    
-    /** 
-     *  Get the total value of all items in the purse.
-     *  @return the total value of items in the purse.
-     */
-    public double getBalance() {
+	/** 
+	 *  Create a purse with a specified capacity.
+	 *  @param capacity is maximum number of coins you can put in purse.
+	 */
+	public Purse( int capacity ) {
+		this.capacity=capacity;
+
+	}
+
+	/**
+	 * Count and return the number of coins in the purse.
+	 * This is the number of coins, not their value.
+	 * @return the number of coins in the purse
+	 */
+	public int count() { return money.size(); }
+
+	/** 
+	 *  Get the total value of all items in the purse.
+	 *  @return the total value of items in the purse.
+	 */
+	public double getBalance() {
 		double sum =0;
 		for(int i =0;i<money.size();i++){
 			sum += money.get(i).getValue();
-			}
+		}
 		return sum;
-		
+
 	}
 
-    
-    /**
-     * Return the capacity of the coin purse.
-     * @return the capacity
-     */
-   
-    public int getCapacity() { 
+
+	/**
+	 * Return the capacity of the coin purse.
+	 * @return the capacity
+	 */
+
+	public int getCapacity() { 
 		return capacity; 
 	}
-    
-    /** 
-     *  Test whether the purse is full.
-     *  The purse is full if number of items in purse equals
-     *  or greater than the purse capacity.
-     *  @return true if purse is full.
-     */
-    public boolean isFull() {
-       if(money.size()>=capacity){
-    	   return true;
-       }else{
-    	   return false;
-       }
-    }
 
-    /** 
-     * Insert a coin into the purse.
-     * The coin is only inserted if the purse has space for it
-     * and the coin has positive value.  No worthless coins!
-     * @param coin is a Coin object to insert into purse
-     * @return true if coin inserted, false if can't insert
-     */
-    public boolean insert( Coin coin ) {
-    	if (!isFull()) {
+	/** 
+	 *  Test whether the purse is full.
+	 *  The purse is full if number of items in purse equals
+	 *  or greater than the purse capacity.
+	 *  @return true if purse is full.
+	 */
+	public boolean isFull() {
+		if(money.size()>=capacity){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	/** 
+	 * Insert a coin into the purse.
+	 * The coin is only inserted if the purse has space for it
+	 * and the coin has positive value.  No worthless coins!
+	 * @param coin is a Coin object to insert into purse
+	 * @return true if coin inserted, false if can't insert
+	 */
+	public boolean insert( Coin coin ) {
+		if (!isFull()) {
 			if (coin.getValue() <= 0) {
 				return false;
 			} else {
@@ -92,28 +92,28 @@ public class Purse {
 			}
 		}
 		return false;
-       
-    }
-    
-    /**  
-     *  Withdraw the requested amount of money.
-     *  Return an array of Coins withdrawn from purse,
-     *  or return null if cannot withdraw the amount requested.
-     *  @param amount is the amount to withdraw
-     *  @return array of Coin objects for money withdrawn, 
+
+	}
+
+	/**  
+	 *  Withdraw the requested amount of money.
+	 *  Return an array of Coins withdrawn from purse,
+	 *  or return null if cannot withdraw the amount requested.
+	 *  @param amount is the amount to withdraw
+	 *  @return array of Coin objects for money withdrawn, 
 	 *    or null if cannot withdraw requested amount.
-     */
-    public Coin[] withdraw( double amount ) {
-    	
-    	List<Coin> withdrawcoin = new ArrayList<Coin>( ); 
+	 */
+	public Coin[] withdraw( double amount ) {
+
+		List<Coin> withdrawcoin = new ArrayList<Coin>( ); 
 		double amountNeededToWithdraw = amount;
-        
-		if(amount<=0){
+
+		if(amount<=0) {
 			return null;
 		}
-		
+
 		Collections.sort(this.money);
-		
+
 		if(amountNeededToWithdraw!=0) {	
 			if(amountNeededToWithdraw <= this.getBalance())
 			{
@@ -126,32 +126,32 @@ public class Purse {
 					}
 				}
 			}
-		
-		if(amountNeededToWithdraw==0)
-		{
-			for(Coin c : withdrawcoin)
+
+			if(amountNeededToWithdraw==0)
 			{
-				money.remove(c);
+				for(Coin c : withdrawcoin)
+				{
+					money.remove(c);
+				}
+			} else {	
+				return null;
 			}
-		}
-		else {	
-			return null;
-		}
-		Coin[] withcoin = new Coin[withdrawcoin.size()];
-		withdrawcoin.toArray(withcoin);
-	
-		return withcoin;
+
+			Coin[] withcoin = new Coin[withdrawcoin.size()];
+			withdrawcoin.toArray(withcoin);
+
+			return withcoin;
 		}
 		return null;
 	}
-  
-    /** 
-     * toString returns a string description of the purse contents.
-     * It can return whatever is a useful description.
-     */
-    public String toString() {
-       return "purse have " + this.count() + " coin with balance " + this.getBalance();
-    }
+
+	/** 
+	 * toString returns a string description of the purse contents.
+	 * It can return whatever is a useful description.
+	 */
+	public String toString() {
+		return "purse have " + this.count() + " coin with balance " + this.getBalance();
+	}
 
 }
 
