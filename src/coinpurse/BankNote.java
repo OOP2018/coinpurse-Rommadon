1f995932d4556c6504440028f2a8581ee5c7545d
GDROM <Rommadon.t@ku.th>
2018-02-01 14:45:21 +0700

    Edit indent and Add JavaDoc

--- src/coinpurse/BankNote.java
+++ src/coinpurse/BankNote.java
@@ -1,26 +1,63 @@
 package coinpurse;
 
-public class BankNote implements Valuable{
-	
+/**
+ * 
+ * Banknote represents value and currency.
+ * 
+ * @author Rommadon Teedo 5810545025
+ */
+public class BankNote implements Valuable {
+
 	private double value;
 	private String currency;
 	private long serialNumber;
 	private static long nextSerialNumber = 1000000;
-	
+
+	/**
+	 * Constructor for banknote with a value and currency
+	 * 
+	 * @param value
+	 *            of the coin
+	 * @param currency
+	 *            of the coin
+	 */
 	public BankNote(double value, String currency) {
 		this.value = value;
 		this.currency = currency;
 		this.serialNumber = nextSerialNumber++;
 	}
+
+	/**
+	 * Return the value of banknote
+	 * 
+	 * @return value of banknote
+	 */
 	public double getValue() {
 		return value;
 	}
+
+	/**
+	 * Return the currency of banknote
+	 * 
+	 * @return currency of banknote
+	 */
 	public String getCurrency() {
 		return currency;
 	}
+
+	/**
+	 * Return the serialNumber of banknote
+	 * 
+	 * @return serialNumber of banknote
+	 */
 	public long getSerial() {
 		return serialNumber;
 	}
+	
+	 /**
+		 * Two serialNumber are equal if they have same value and currency.
+		 */
+
 	public boolean equals(Object obj) {
 		if (obj == null)
 			return false;
@@ -29,10 +66,13 @@ public class BankNote implements Valuable{
 		BankNote other = (BankNote) obj;
 		return this.value == other.value && this.currency == other.currency;
 	}
+
+	 /**
+		 * show value and currency
+		 * @return the sentence value and currency.
+		 */
 	public String toString() {
 		return String.format("%.0f-%s note[%d]", value, currency, serialNumber);
 	}
-
-	
 
 }
--- src/coinpurse/ValueComparator.java
+++ src/coinpurse/ValueComparator.java
@@ -1,24 +1,38 @@
 package coinpurse;
-
 import java.util.Comparator;
 
-public class ValueComparator implements Comparator<Valuable>{
+/**
+ * 
+ * Compare value of money.
+ * 
+ * @author Rommadon Teedo 5810545025
+ */
+
+public class ValueComparator implements Comparator<Valuable> {
+	 /**
+		 * Compare the value of coins.
+		 * @param a is Valuable money
+		 * @param b is Valuable money
+		 * @return 0 if equal 1 if a >b and -1 if a<b
+		 */
+	   
 	public int compare(Valuable a, Valuable b) {
 		if (a.getCurrency().equals(b.getCurrency())) {
-			if (a.getValue() == b.getValue()){
+			if (a.getValue() == b.getValue()) {
 				return 0;
 			}
-			if (a.getValue() > b.getValue()){
+			if (a.getValue() > b.getValue()) {
 				return 1;
 			}
-			if (a.getValue() < b.getValue()){
+			if (a.getValue() < b.getValue()) {
 				return -1;
 			}
-		}if(a.getCurrency().charAt(0) > b.getCurrency().charAt(0)){
+		}
+		if (a.getCurrency().charAt(0) > b.getCurrency().charAt(0)) {
 			return 1;
-		}else {
+		} else {
 			return -1;
-		
+
 		}
 	}
 
